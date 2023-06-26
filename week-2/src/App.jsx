e8c8d1f94a3879b6581308d5d30ce735a045d343
Haru Im <chalk323@naver.com>
2023-06-27 01:30:38 +0900

    usememo1

--- week-2/src/App.jsx
+++ week-2/src/App.jsx
@@ -1,43 +1,22 @@
 import "./App.css";
-import { useCallback, useState } from "react";
-import Box1 from "./react-hooks/react.memo/Box1";
-import Box2 from "./react-hooks/react.memo/Box2";
-import Box3 from "./react-hooks/react.memo/Box3";
-import NewBox1 from "./react-hooks/usecallback/newBox1";
-
-const boxesStyle = {
-  display: "flex",
-  marginTop: "10px",
-};
+import HeavyComponent from "./usememo/HeavyComponent";
 
 function App() {
-  const [count, setCount] = useState(0);
-  console.log("app component rendered");
-
-  const onPlusButtonClickHandler = () => {
-    setCount(count + 1);
+  const navStyleObj = {
+    backgroundColor: "yellow",
+    marginBottom: "30px",
   };
 
-  const onMinusButtonClickHandler = () => {
-    setCount(count + 1);
+  const footerStyleObj = {
+    backgroundColor: "green",
+    marginTop: "30px",
   };
-
-  const initCount = useCallback(() => {
-    console.log(`[COUNT 변경] ${count}에서 0으로 변경되었습니다.`);
-    setCount(0);
-  }, [count]);
 
   return (
     <>
-      <h3>카운트 예제입니다!</h3>
-      <p>현재 카운트 : {count}</p>
-      <button onClick={onPlusButtonClickHandler}>+</button>
-      <button onClick={onMinusButtonClickHandler}>-</button>
-      <div style={boxesStyle}>
-        <NewBox1 initCount={initCount} />
-        <Box2 />
-        <Box3 />
-      </div>
+      <nav style={navStyleObj}>네비게이션 바</nav>
+      <HeavyComponent />
+      <footer style={footerStyleObj}>푸터 영역이에요</footer>
     </>
   );
 }
