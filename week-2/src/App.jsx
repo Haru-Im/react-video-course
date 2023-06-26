8244a9998a7a6a74621a93f26627c6e2f378b365
Haru Im <chalk323@naver.com>
2023-06-27 01:21:14 +0900

    usecallback

--- week-2/src/App.jsx
+++ week-2/src/App.jsx
@@ -1,8 +1,9 @@
 import "./App.css";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import Box1 from "./react-hooks/react.memo/Box1";
 import Box2 from "./react-hooks/react.memo/Box2";
 import Box3 from "./react-hooks/react.memo/Box3";
+import NewBox1 from "./react-hooks/usecallback/newBox1";
 
 const boxesStyle = {
   display: "flex",
@@ -21,17 +22,20 @@ function App() {
     setCount(count + 1);
   };
 
+  const initCount = useCallback(() => {
+    console.log(`[COUNT 변경] ${count}에서 0으로 변경되었습니다.`);
+    setCount(0);
+  }, [count]);
+
   return (
     <>
-      <h3>카운트 예제</h3>
+      <h3>카운트 예제입니다!</h3>
       <p>현재 카운트 : {count}</p>
       <button onClick={onPlusButtonClickHandler}>+</button>
       <button onClick={onMinusButtonClickHandler}>-</button>
       <div style={boxesStyle}>
-        <Box1 />
-
+        <NewBox1 initCount={initCount} />
         <Box2 />
-
         <Box3 />
       </div>
     </>
