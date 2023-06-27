ddd8715fd22dfecaf69679b77ac63c97b2b051dd
Haru Im <chalk323@naver.com>
2023-06-27 15:43:37 +0900

    redux counter refactoring

--- week-2/src/App.jsx
+++ week-2/src/App.jsx
@@ -1,15 +1,10 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { minusOne, plusOne } from "./redux/modules/counter";
 
 const App = () => {
-  // 여기에서 store에 접근하여 counter의 값을 읽어올 때 => useSelector
-  // 여기서 state : 중앙저장소 전체의 state
-  const data = useSelector((state) => state);
-
-  //counter만 가지고 오고 싶을 때
   const counter = useSelector((state) => state.counter);
 
-  //dispatch 가져오기
   const dispatch = useDispatch();
 
   console.log("counter -> ", counter.number);
@@ -19,19 +14,14 @@ const App = () => {
       현재 카운트: {counter.number} <br />
       <button
         onClick={() => {
-          //+1을 해주는 로직
-          dispatch({
-            type: "PLUS_ONE",
-          });
+          dispatch(plusOne());
         }}
       >
         +
       </button>
       <button
         onClick={() => {
-          dispatch({
-            type: "MINUS_ONE",
-          });
+          dispatch(minusOne());
         }}
       >
         -
--- week-2/src/redux/modules/counter.js
+++ week-2/src/redux/modules/counter.js
@@ -1,3 +1,21 @@
+// action Value
+// 상수는 보통 대문자로 작성
+export const PLUS_ONE = "counter/PLUS_ONE";
+export const MINUS_ONE = "counter/MINUS_ONE";
+
+// action creator : action value를 return하는 함수
+export const plusOne = () => {
+  return {
+    type: PLUS_ONE,
+  };
+};
+
+export const minusOne = () => {
+  return {
+    type: MINUS_ONE,
+  };
+};
+
 //초기 상태값(state)
 const initialState = {
   number: 0,
@@ -10,11 +28,11 @@ const initialState = {
 //action에는 type과 value가 있음
 const counter = (state = initialState, action) => {
   switch (action.type) {
-    case "PLUS_ONE":
+    case PLUS_ONE:
       return {
         number: state.number + 1,
       };
-    case "MINUS_ONE":
+    case MINUS_ONE:
       return {
         number: state.number - 1,
       };
