f1b7b42c93b8e47bf8aae1c4b71997ae0d428f69
Haru Im <chalk323@naver.com>
2023-06-27 14:10:40 +0900

    redux 설정 : counter - fix

--- week-2/src/App.jsx
+++ week-2/src/App.jsx
@@ -4,6 +4,8 @@ import { useSelector } from "react-redux";
 const App = () => {
   // 여기에서 store에 접근하여 counter의 값을 읽어올 때 => useSelector
   // 여기서 state : 중앙저장소 전체의 state
+  const data = useSelector((state) => state);
+  console.log(data);
 
   //counter만 가지고 오고 싶을 때
   const counterStore = useSelector((state) => state.counter);
